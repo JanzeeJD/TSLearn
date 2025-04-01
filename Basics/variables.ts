60a04b4f89826156c5b8ca6b0d6e01d2c0c64f60
Jancy Daniel <jancyjaya12@gmail.com>
2025-04-01 22:17:04 +0530

    feat: first task (variables)

---
+++ Basics/variables.ts
@@ -0,0 +1,16 @@
+/*Task 1 – variables.ts
+Create 3 variables: userName, userAge, isLoggedIn
+
+Give them types: string, number, boolean
+
+Print a sentence:
+"Hello, my name is John. I am 25 years old. Logged in: true" */
+
+
+let userName : string = "MoanaUs";
+let userAge : number = 16;
+let isLoggedIn : boolean = true;
+
+console.log(`"hello, my name is ${userName}. I am ${userAge} years old. Logged in : ${isLoggedIn}."`);
+
+
