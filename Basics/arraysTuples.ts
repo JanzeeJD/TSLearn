b3628f1f883348b8ecdc8c613c7ca7ed7c90c7e3
Jancy Daniel <jancyjaya12@gmail.com>
2025-04-02 21:35:21 +0530

    feat: task2 (arrayTuples)

---
+++ Basics/arraysTuples.ts
@@ -0,0 +1,14 @@
+/* Array: A list of items of the same type.
+
+Tuple: A fixed-length list with specific types. */
+
+/* Create arraysTuples.ts:
+
+Make an array of your 3 favorite colors.
+
+Make a tuple of your name and your birth year. */
+
+let colors : string [] = ["White","Blue","Green"];
+let girl : [string,number] = ["Moana",2014]; // Tuple
+
+console.log(colors);
