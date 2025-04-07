030b707c0f9d339c1de9ab25785e35b75f55e6e0
Jancy Daniel <jancyjaya12@gmail.com>
2025-04-07 20:02:59 +0530

    feat: task7 (unionType)

---
+++ Basics/unionType.ts
@@ -0,0 +1,20 @@
+/* Union types let a variable hold more than one type.
+Use | (pipe) symbol to define multiple allowed types.
+ */
+
+/*
+Create a variable userId that can be a number or string.
+
+Assign a number to it and print it.
+
+Then assign a string to it and print again.
+ */
+
+let userId : number | string ;
+
+userId = 17;
+userId = "JD1708"
+
+console.log(userId);
+
+ 
