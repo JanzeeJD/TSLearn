76de089919a53dc24faf58e8c386849c92792e9c
Jancy Daniel <jancyjaya12@gmail.com>
2025-04-10 21:58:20 +0530

    task1(typeNarrowing)

---
+++ Medium/typeNarrowing.ts
@@ -0,0 +1,10 @@
+/* Type narrowing is how TypeScript figures out the exact type of a variable when there are 
+multiple possible types (like in union types). */
+
+/* Create a function logValue(value: number | string) that:
+
+If the value is a number, multiplies it by 2.
+
+If it’s a string, prints it in uppercase.
+
+Call this function with both a number and a string. */
