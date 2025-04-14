fd9980b707745932bac4ae5685e463708592f9b1
Jancy Daniel <jancyjaya12@gmail.com>
2025-04-14 21:51:21 +0530

    feat: optional Chaining and Nullish Coalescing

---
+++ Medium/optionalNullish.ts
@@ -0,0 +1,8 @@
+/* Optional Chaining ?.
+It allows you to access deeply nested properties without checking each level.
+If something is undefined or null, it stops and returns undefined instead of throwing an error.
+eg:- console.log(user?.profile?.name); // won't throw if profile is undefined */
+
+/* Nullish Coalescing ??
+It gives you a default value when the left-hand side is null or undefined. 
+eg:-  let name = userName ?? "Guest"; // use "Guest" if userName is null or undefined */
